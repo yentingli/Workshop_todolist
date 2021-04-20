f4cc9ad96aa67505e033e7ca8134cbe6a5f9d12c
Yen-Ting Li <bluejack.ac96@gmail.com>
2021-04-20 14:41:48 +0800

    done: feature3/task-add (#5)

--- js-todo-list/main.js
+++ js-todo-list/main.js
@@ -1,15 +1,27 @@
-// TO DO
 document.addEventListener('DOMContentLoaded', function () {
   document.querySelector('ul').addEventListener('click', (e) =>{
+    // feature1
     e.target.classList.toggle("checked")
     
+    // feature2
     let li_span = document.querySelectorAll('li>span')
-    console.log(li_span.len)
     
     for (i = 0; i < li_span.length; ++i) {
       if (li_span[i].classList.contains("checked")){
         li_span[i].parentNode.remove()
+        return null
       }
     }
+    
+  })
+
+  // feature3
+  document.querySelector('#addBtn').addEventListener('click', (e) =>{
+    task_text = e.target.previousElementSibling.value
+    let task_list = document.querySelector('ul')
+    const task_li = document.createElement("li")
+    task_li.innerHTML = task_text+ '<span class="close">x</span>'
+    task_list.insertAdjacentElement('afterbegin', task_li)
+    e.target.previousElementSibling.value = ''
   })
 })
