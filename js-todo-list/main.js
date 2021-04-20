9f7f737fa7b4dcd757273f3acde6173724370090
Yen-Ting Li <bluejack.ac96@gmail.com>
2021-04-20 11:58:33 +0800

    Revert "done: task-check function"
    This reverts commit 72ca891e347ff1e0a7e0111de96741e8a748f6ce.

--- js-todo-list/main.js
+++ js-todo-list/main.js
@@ -1,6 +1,1 @@
 // TO DO
-document.addEventListener('DOMContentLoaded', function () {
-  document.querySelector('ul').addEventListener('click', (e) =>{
-    e.target.classList.toggle("checked") 
-  })
-})
