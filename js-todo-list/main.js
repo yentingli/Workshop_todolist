bc0b2bc5e69eb2d1288dd958554e2d567a4cb650
Yen-Ting Li <bluejack.ac96@gmail.com>
2021-04-20 13:47:04 +0800

    Feature2/task delete (#4)
    * WIP
    
    * task delet done

--- js-todo-list/main.js
+++ js-todo-list/main.js
@@ -1,6 +1,15 @@
 // TO DO
 document.addEventListener('DOMContentLoaded', function () {
   document.querySelector('ul').addEventListener('click', (e) =>{
-    e.target.classList.toggle("checked") 
+    e.target.classList.toggle("checked")
+    
+    let li_span = document.querySelectorAll('li>span')
+    console.log(li_span.len)
+    
+    for (i = 0; i < li_span.length; ++i) {
+      if (li_span[i].classList.contains("checked")){
+        li_span[i].parentNode.remove()
+      }
+    }
   })
 })
